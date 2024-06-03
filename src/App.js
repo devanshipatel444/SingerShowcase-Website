ae0f784cfc07229897e00e292ec0efaf16fe4706
devanshipatel444 <122767601+devanshipatel444@users.noreply.github.com>
2024-06-03 18:33:08 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+
+  
+
+  
+
+
+  return (
+    
+    <React.Fragment>
+      
+      <img src="Pictures/Room/back-wall.png" className="backWall"/> 
+      <img src="Pictures/Room/side-wall.png" className="leftWall"/> 
+      <img src="Pictures/Room/side-wall-2.png" className="rightWall"/> 
+
+    </React.Fragment> 
+
+  );
+};
+
+    
+    
+export default App;
+
+
+
